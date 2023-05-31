447e56021391fc60a9f9945c4397b3a1f2871ac4
KAVYABAI RG <85789613+kavyaBAI@users.noreply.github.com>
2023-05-31 18:38:44 +0530

    Create listoflist_unique_synonym.py

---
+++ listoflist_unique_synonym.py
@@ -0,0 +1,27 @@
+Task- create 2 tables insert the values for the given input get the listoflist without duplicate values.
+
+import mysql.connector
+
+
+def connection(user):
+  khost,kname,kpass,kdb=user.split(#)
+   my_conn=mysql.connector.connect(Host=khost,user=kname,paswd=kpass,databse=kdb)
+   con=my_conn.cursor()
+   con.close()
+   my_conn.close()
+    return con,my_conn
+    
+  
+  
+  
+  
+  
+ def task():
+  
+  
+  
+  
+  
+  if ___name__=='__main__':
+    db_connection=localhost#root#kavya8787@#info5
+    connection(db_connection)
