6baf1d9797be2f15cfeb34dedebf8457afb62940
KAVYABAI RG <85789613+kavyaBAI@users.noreply.github.com>
2023-06-01 10:13:11 +0530

    Update listoflist_unique_synonym.py

--- listoflist_unique_synonym.py
+++ listoflist_unique_synonym.py
@@ -6,22 +6,22 @@ import mysql.connector
 def connection(user):
   khost,kname,kpass,kdb=user.split(#)
    my_conn=mysql.connector.connect(Host=khost,user=kname,paswd=kpass,databse=kdb)
-   con=my_conn.cursor()
-   con.close()
+   cur=my_conn.cursor()
+   cur.close()
    my_conn.close()
     return con,my_conn
-    
   
-  
-  
-  
-  
- def task():
-  
-  
-  
-  
+ def task(db_connection):
+    connection(db_connection)
+    sql="select drug_id from drugtable"
+    cur.excute(sql)
+    res=cur.fetchone()
+    print(res) # 54
+    sql="select sysnoms from the synonym where drug_id=%s"
+    cur.execute(sql,%res)
+    cur.fetchall()
+     
   
   if ___name__=='__main__':
-    db_connection=localhost#root#kavya8787@#info5
-    connection(db_connection)
+    db_connection=localhost#root#kavya8787@#info
+    task()
