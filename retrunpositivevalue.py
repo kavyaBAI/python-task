0c8ab378889391834c02e0def3f395350e0f3f11
KAVYABAI RG <85789613+kavyaBAI@users.noreply.github.com>
2023-06-30 16:18:43 +0530

    Create retrunpositivevalue.py

---
+++ retrunpositivevalue.py
@@ -0,0 +1,16 @@
+import math
+def find(num):
+  val = abs(num)
+  res = val.ceil(val)
+  return(res)
+fing(-249.09) 
+
+#for string many methods 
+.isdigit()
+.isdecimal()
+.join()
+.strip()
+.split()
+.reverse()
+.sorted()
+.coount()
